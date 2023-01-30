8866f8892831fb2164eaa034a2d9f36e4a2de6e2
roddersnz21 <roddersnz@gmail.com>
2023-01-30 09:59:41 +0000

    save test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,54 @@
 class Calculator {
 	constructor(previousOperandTextElement, currentOperandTextElement) {
+	  // store the element references for updating the display	
 	  this.previousOperandTextElement = previousOperandTextElement
 	  this.currentOperandTextElement = currentOperandTextElement
+	  // call the clear function to set initial values
 	  this.clear()
 	}
   
+	// function to clear all values
 	clear() {
 	  this.currentOperand = ''
 	  this.previousOperand = ''
 	  this.operation = undefined
 	}
   
+	// function to delete last character from current operand
 	delete() {
 	  this.currentOperand = this.currentOperand.toString().slice(0, -1)
 	}
   
+	// function to append a number to the current operand
 	appendNumber(number) {
+	  // prevent adding more than one decimal point
 	  if (number === '.' && this.currentOperand.includes('.')) return
 	  this.currentOperand = this.currentOperand.toString() + number.toString()
 	}
   
+	// function to choose an operation
 	chooseOperation(operation) {
+	  // prevent choosing operation if there's no current operand
 	  if (this.currentOperand === '') return
+	  // if there's a previous operand, compute it first
 	  if (this.previousOperand !== '') {
 		this.compute()
 	  }
+	  // store the operation
 	  this.operation = operation
+	  // store the current operand as previous operand
 	  this.previousOperand = this.currentOperand
 	  this.currentOperand = ''
 	}
   
+	// function to perform the computation
 	compute() {
 	  let computation
 	  const prev = parseFloat(this.previousOperand)
 	  const current = parseFloat(this.currentOperand)
+	  // return if either prev or current is not a number
 	  if (isNaN(prev) || isNaN(current)) return
+	  // perform the computation based on the chosen operation
 	  switch (this.operation) {
 		case '+':
 		  computation = prev + current
@@ -51,11 +65,14 @@ class Calculator {
 		default:
 		  return
 	  }
+	  
+	  // update the current operand with the result of the computation
 	  this.currentOperand = computation
 	  this.operation = undefined
 	  this.previousOperand = ''
 	}
   
+	// function to format the display number
 	getDisplayNumber(number) {
 	  const stringNumber = number.toString()
 	  const integerDigits = parseFloat(stringNumber.split('.')[0])
